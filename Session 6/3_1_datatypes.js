03fe9e42bf22ca726fa6aeff0a7bed90cc820bb0
bassanttmahmoud <bassantmahmoud909@gmail.com>
2024-08-29 05:14:40 +0300

    session 6

---
+++ Session 6/3_1_datatypes.js
@@ -0,0 +1,48 @@
+/*
+  Data Types Intro
+  - String
+  - Number
+  - Array => Object
+  - Object
+  - Boolean
+*/
+  //                 primitive: 
+
+// number
+age = 25; // Integer
+temperature = 98.6; // Floating-point number
+
+// string
+let greeting = 'Hello, World!'; // Single quotes
+let name = "Alice"; // Double quotes
+
+
+// Boolean
+
+let isActive = true; // Boolean true
+let isDeleted = false; // Boolean false
+
+// undefined
+let someValue; // Declared but not initialized
+// console.log(someValue); // Output: undefined
+
+// // null
+let user = null; // Explicitly setting a variable to have no value
+// console.log( user)
+
+
+
+//         non primitive
+
+// Object Literal
+
+var person = {
+name: 'Alice',
+age: 25,
+};
+// console.log(person); 
+
+// Array
+
+var person = ['Alice ' , 25];
+// console.log(person); 
