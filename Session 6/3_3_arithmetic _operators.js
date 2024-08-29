935b22b5c4684e0b10ec888816f92b2524bc39b9
bassanttmahmoud <bassantmahmoud909@gmail.com>
2024-08-29 06:39:34 +0300

    session 6

---
+++ Session 6/3_3_arithmetic _operators.js
@@ -0,0 +1,44 @@
+
+// Addition
+var x = 100,
+  y = 200,
+  z = x + y; // z = 100 + 200 = 300
+
+// Subtraction
+var x = 1000,
+  y = 150,
+  z = x - y; // z = 1000 - 150 = 850
+
+// String And Number
+
+var x = "name",
+  y = 150,
+  z = x - y; // z = "Osama" - 150 = NAN (not a number)
+// console.log(z);
+
+// Divide
+
+var x = 1000,
+  y = 150,
+  z = x / y;
+
+// Multiplication
+
+var x = 1000,
+  y = 20,
+  z = x * y; // z = 1000 * 20 = 20000
+
+// Modulus
+
+var x = 20,
+  y = 2,
+  z = x % y; // z = 20 % 2 = 0
+
+
+
+// Increment
+
+var myCounter = 1;
+
+// console.log(myCounter++);
+// console.log(myCounter);
---
+++ Session 6/3_4_operators.js
@@ -0,0 +1,32 @@
+//                assignment operators
+
+let x = 100;
+x /= 5;
+// console.log(x);
+
+
+let text = "Hello"; text += " World";
+// console.log(text);
+
+
+//               comparison operators
+
+a = 20 ;
+b = 30 ;
+// console.log (a>b);
+
+//               logical operators
+
+a = 20 ;
+b = 30 ;
+// console.log (a<b && a>10); // and operator
+
+// console.log (a<b || a==20 ); // or operator
+
+// console.log (!(a==b) ); // not operator
+
+
+
+
+
+
