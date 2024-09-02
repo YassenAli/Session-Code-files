ddf274f1fee6d72838e47018a539afe84a4b30b5
bassanttmahmoud <bassantmahmoud909@gmail.com>
2024-09-02 06:45:45 +0300

    session 7

--- Session 7/2_3_number.js
+++ Session 7/2_3_number.js
@@ -1,12 +1,25 @@
 /*
   Number Methods
+
+  
   - toString()
-  - toFixed()
-  - parseInt()
+  - toFixed(2)
+  - parseInt()  //betrg3ly rakam mn west string w rakam 3aks number function
   - parseFloat()
   - isInteger() [ES6]
   - isNaN() [ES6]
 */
 
 
-console.log(Number.parseInt(100))
+console.log((100).toString());
+console.log(100.554555.toFixed(2));
+
+// console.log(Number("100 name"));
+// console.log(parseInt("100 name "));
+// console.log(parseFloat("100.500 name"));
+
+// console.log(Number.isInteger("100"));
+// console.log(Number.isInteger(100.500));
+// console.log(Number.isInteger(100));
+
+// console.log(Number.isNaN("name" + 20));
